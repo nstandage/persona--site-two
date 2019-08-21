674b436e1da1500cf47bc9b2c3fe06cdbd87c912
Nathan <nathan@Nathans-MacBook-Pro-2.local>
2019-08-20 19:44:00 -0600

    added penny wise and test prep pages as well as detail css. I also restructured the app set up Gulp.

---
+++ app/scripts/detail-scripts.js
@@ -0,0 +1,27 @@
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// create a scene
+var contactScene = new ScrollMagic.Scene({
+  triggerElement: ".dark",
+  triggerHook: 0.78,
+  reverse: true
+})
+  .setClassToggle(".contactContainer", "contactContainerDark")
+  .addTo(controller); // assign the scene to the controller
+
+var menuScene = new ScrollMagic.Scene({
+  triggerElement: ".dark",
+  triggerHook: 0.1,
+  reverse: true
+})
+  .setClassToggle(".menu", "darkMenu")
+  .addTo(controller);
+
+var logoScene = new ScrollMagic.Scene({
+  triggerElement: ".dark",
+  triggerHook: 0.05,
+  reverse: true
+})
+  .setClassToggle(".logo", "darkLogo")
+  .addTo(controller);
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+//https://www.youtube.com/watch?v=QgMQeLymAdU
+"use strict";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+
+//compile scss into css
+
+function style() {
+  return gulp
+    .src("./app/styles/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./app/styles/css"))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "app/"
+      // index: "test-prep.html"
+    }
+  });
+  gulp.watch("./app/styles/scss/**/*.scss", style);
+  gulp.watch("./app/*.html").on("change", browserSync.reload);
+  gulp.watch("./app/scripts/*.js").on("change", browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
